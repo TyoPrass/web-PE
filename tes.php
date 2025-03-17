8b21c968cbe6c97876d73ca5ddbda486cd93a8e9
TyoPrass <220441100072@student.trunojoyo.ac.id>
2025-03-17 15:43:21 +0700

    kurang pic target & keterangan

--- tes.php
+++ tes.php
@@ -1,3 +1,8 @@
 <?php
 // halo saya mau coba upload lewat sini hehe
+//*
+// ternyata pada bagian beberapa data tidak dapat melakukan update dengan baik
+/** 
+ * harus segera diperbaiki dengan cepat
+ */
 ?>
