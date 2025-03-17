34d0f985ab3a1d90223e750dae9708a0dff09f22
TyoPrass <220441100072@student.trunojoyo.ac.id>
2025-03-17 07:58:27 +0700

    perbarui

--- assets/js/pages/demo.quilljs.js
+++ assets/js/pages/demo.quilljs.js
@@ -1,8 +1,1 @@
 var quill=new Quill("#snow-editor",{theme:"snow",modules:{toolbar:[[{font:[]},{size:[]}],["bold","italic","underline","strike"],[{color:[]},{background:[]}],[{script:"super"},{script:"sub"}],[{header:[!1,1,2,3,4,5,6]},"blockquote","code-block"],[{list:"ordered"},{list:"bullet"},{indent:"-1"},{indent:"+1"}],["direction",{align:[]}],["link","image","video"],["clean"]]}}),quill=new Quill("#bubble-editor",{theme:"bubble"});
-<<<<<<< HEAD
-var quill2=new Quill2("#snow-editor2",{theme:"snow2",modules:{toolbar:[[{font:[]},{size:[]}],["bold","italic","underline","strike"],[{color:[]},{background:[]}],[{script:"super"},{script:"sub"}],[{header:[!1,1,2,3,4,5,6]},"blockquote","code-block"],[{list:"ordered"},{list:"bullet"},{indent:"-1"},{indent:"+1"}],["direction",{align:[]}],["link","image","video"],["clean"]]}}),quill2=new Quill2("#bubble-editor2",{theme:"bubble2"});
-=======
-var quill2=new Quill2("#snow-editor2",{theme:"snow2",modules:{toolbar:[[{font:[]},{size:[]}],["bold","italic","underline","strike"],[{color:[]},{background:[]}],[{script:"super"},{script:"sub"}],[{header:[!1,1,2,3,4,5,6]},"blockquote","code-block"],[{list:"ordered"},{list:"bullet"},{indent:"-1"},{indent:"+1"}],["direction",{align:[]}],["link","image","video"],["clean"]]}}),quill2=new Quill2("#bubble-editor2",{theme:"bubble2"});
-
-
->>>>>>> 4a71b659b3b653c927a6d1413733ba9f978f1e9d
