092d2fad47768edc6ee267fb983eca08682ba89c
TyoPrass <220441100072@student.trunojoyo.ac.id>
2025-03-18 16:06:14 +0700

    masalah terdapat dalam mat spec dan mat size edit case open lagi

--- tes.php
+++ tes.php
@@ -6,5 +6,10 @@
  * harus segera diperbaiki dengan cepat
  * karena sebelum tgl tertentu harus selesai
  * 
+ * Ayo buat laman schedule 
+ * habis itu dimasukkan semuanya kedalam database
+ * lalu jadikan semuanya dalam bentuk tampilan template yang terdapat dalam bootstrap
+ * jangan lupa dihosting
+ * 
  */
 ?>
